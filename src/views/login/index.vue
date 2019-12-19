810737c4bd42d969fb50633c41b9ea5f7596066e
xuan111114444444444 <87555555@qq.com>
2019-12-19 13:34:39 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,20 +5,20 @@
             <img src="../../assets/img/logo_index.png" alt="">
         </div>
         <!-- 表单 -->
-        <el-form>
+        <el-form :model="loginForm" :rules="loginRules" ref="LoginRef">
             <!-- 表单域 -->
-            <el-form-item>
-                <el-input v-model="input" placeholder="请输入手机号"></el-input>
+            <el-form-item  prop="mobile">
+                <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
             </el-form-item>
-            <el-form-item>
-                <el-input v-model="input" placeholder="验证码" class="inputId"></el-input>
+            <el-form-item prop="code">
+                <el-input v-model="loginForm.code" placeholder="验证码" class="inputId"></el-input>
                 <el-button plain class="btn">发送验证码</el-button>
             </el-form-item>
-            <el-form-item>
-                 <el-checkbox v-model="checked">我已读并同意用户协议和隐私条款</el-checkbox>
+            <el-form-item  prop="agree">
+                 <el-checkbox v-model="loginForm.agree">我已读并同意用户协议和隐私条款</el-checkbox>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" style="width:100%">主要按钮</el-button>
+                <el-button type="primary" style="width:100%" @click="ruleForm">登录</el-button>
             </el-form-item>
 
         </el-form>
@@ -28,7 +28,41 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        agree: false // 是否同意协议
+      },
+      loginRules: {
+        mobile: [{ required: true, message: '请输入手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确' }],
+        code: [{ required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '手机号格式不正确' }
+        ],
+        agree: [
+          { validator: function (rule, value, callback) {
+            if (value) {
+              callback()
+            } else {
+              callback(new Error('未同意 '))
+            }
+          }
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    ruleForm () {
+      this.$refs.LoginRef.validate(function (isOk) {
+        if (isOk) {
+          console.log('校验成功')
+        }
+      })
+    }
+  }
 }
 </script>
 
