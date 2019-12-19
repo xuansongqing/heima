cbc8dbdb9bcebe491bfed857ddbe6eb71aea3eb9
xuan111114444444444 <87555555@qq.com>
2019-12-19 10:09:42 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home'
+import Login from '../views/login'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    redirect: '/home'
+
+  },
+  {
+    path: '/home',
+    // name: 'home',
     component: Home
+  },
+  {
+    path: '/login',
+    component: Login
   }
   // {
   //   path: '/about',
