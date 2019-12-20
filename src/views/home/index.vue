6c68236e161572f8f1564d51fd90a95ddb385964
xuan111114444444444 <87555555@qq.com>
2019-12-20 14:39:10 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,11 @@
 <template>
-<div>主页</div>
+    <el-container>
+        <el-aside>左侧栏</el-aside>
+        <el-container>
+            <el-header>头部</el-header>
+            <el-main>内容区域</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
