ec43e70fc98cace2e1881301a6bc2c65a1719ec1
xuan111114444444444 <87555555@qq.com>
2019-12-20 15:56:28 +0800

    黑马头条PC-主页模块-左侧导航菜单

---
+++ src/components/home/zuoce.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="zuoce">
+  <div  class="title">
+    <img src="../../assets/img/logo_admin.png" alt="">
+  </div>
+  <el-menu background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
+      <el-menu-item>
+        <span slot="title">首页</span>
+      </el-menu-item>
+      <el-submenu index="1">
+         <template slot="title">
+           <span slot="title">内容管理</span>
+         </template>
+        <el-menu-item>发布文章</el-menu-item>
+        <el-menu-item>内容列表</el-menu-item>
+        <el-menu-item>评论管理</el-menu-item>
+        <el-menu-item>素材管理</el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+         <template slot="title">
+           <span slot="title">粉丝管理</span>
+         </template>
+        <el-menu-item>图文数据</el-menu-item>
+        <el-menu-item>粉丝概况</el-menu-item>
+        <el-menu-item>粉丝画像</el-menu-item>
+        <el-menu-item>粉丝列表</el-menu-item>
+      </el-submenu>
+      <el-menu-item>账户信息</el-menu-item>
+  </el-menu>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+  <style lang="less" scoped>
+  .zuoce{
+    width: 230px;
+     .el-menu{
+       border-right: none;
+      }
+  }
+  .title{
+    text-align: center;
+    background-color:#2e2f32;
+    padding: 15px 0;
+    img{
+      height: 30px;
+    }
+  }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,8 @@
 <template>
     <el-container>
-        <el-aside>左侧栏</el-aside>
+        <el-aside style="height:100vh;background-color:#353b4e;width:230px">
+          <zuoce></zuoce>
+        </el-aside>
         <el-container>
             <el-header>头部</el-header>
             <el-main>内容区域</el-main>
@@ -9,8 +11,11 @@
 </template>
 
 <script>
+import zuoCe from '../../components/home/zuoce'
 export default {
-
+  components: {
+    'zuoce': zuoCe
+  }
 }
 </script>
 
