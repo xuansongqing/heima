320757d7dd958b2664a976c7179897f4d9b5bc8d
xuan111114444444444 <87555555@qq.com>
2019-12-22 09:21:40 +0800

    黑马头条PC-面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import zuoCe from './home/zuoce'
 import header from './home/header'
+import breadCrumbs from '../common/bread-crumbs.vue'
 
 export default {
   install (Vue) {
     Vue.component('zuoce', zuoCe)
     Vue.component('header-top', header)
+    Vue.component('bread-crumbs', breadCrumbs)
   }
 
 }
