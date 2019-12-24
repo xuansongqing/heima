4986143ff8bfb1cd4b7ffff6be315fc821c5802c
xuan111114444444444 <87555555@qq.com>
2019-12-24 19:56:12 +0800

    黑马头条PC-内容列表-新建页面-挂载路由

---
+++ src/views/articles/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-card>
+       <bread-crumbs slot="header">
+           <template slot="title">内容列表</template>
+       </bread-crumbs>
+  </el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
