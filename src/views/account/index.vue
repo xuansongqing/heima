1a96edb2a0099615dc154c43373272745cfff916
xuan111114444444444 <87555555@qq.com>
2019-12-29 18:59:53 +0800

    黑马头条PC-账户信息-用户数据保存

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -3,26 +3,26 @@
         <bread-crumbs slot="header">
             <template slot="title">账户信息</template>
         </bread-crumbs>
-        <el-form label-width='100px'>
-            <el-form-item label="头像 :">
+        <el-form label-width='100px' :model="formData" :rules="rules" ref="myForm">
+            <el-form-item label="头像 :" prop='photo'>
                 <el-upload  action='' :show-file-list='false' class="upload-img">
                   <img :src="formData.photo ? formData.photo : defaultImg" alt="">
                 </el-upload>
             </el-form-item>
-            <el-form-item label='用户名 :'>
+            <el-form-item label='用户名 :' prop='name'>
                 <el-input v-model="formData.name" style="width:60%"></el-input>
             </el-form-item>
-            <el-form-item label='简介 :'>
+            <el-form-item label='简介 :' prop='intro'>
                  <el-input v-model="formData.intro" style="width:60%"></el-input>
             </el-form-item>
-            <el-form-item label='邮箱:'>
+            <el-form-item label='邮箱:' prop='email'>
                  <el-input v-model="formData.email" style="width:60%"></el-input>
             </el-form-item>
-            <el-form-item label='手机号 :'>
+            <el-form-item label='手机号 :' prop='mobile'>
                  <el-input v-model="formData.mobile" style="width:60%" disabled></el-input>
             </el-form-item>
             <el-form-item >
-                <el-button type="primary">保存信息</el-button>
+                <el-button type="primary" @click="saveUserInfo">保存信息</el-button>
             </el-form-item>
         </el-form>
 
@@ -40,10 +40,32 @@ export default {
         mobile: '',
         photo: ''
       },
+      rules: {
+        name: [{ required: 'true', message: '请输入用户名称' },
+          { min: 1, max: 8, message: '长度1至8个字符' }],
+        email: [{ required: 'true', message: '请输入邮箱地址' },
+          { pattern: /^([0-9A-Za-z\-.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g, message: '邮箱格式不正确' }]
+      },
       defaultImg: require('../../assets/img/beijing.jpg')
     }
   },
   methods: {
+    // 保存用户信息
+    saveUserInfo () {
+      this.$refs.myForm.validate().then(() => {
+        this.$axios({
+          url: '/user/profile',
+          method: 'patch',
+          data: this.formData
+        }).then(result => {
+          this.$message({
+            type: 'success',
+            message: '保存成功'
+          })
+        })
+      })
+    },
+    // 获取用户信息
     getUserInfo () {
       this.$axios({
         url: '/user/profile'
