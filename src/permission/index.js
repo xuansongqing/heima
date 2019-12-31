dd4e185e19c1f47eaebdf05d12c6adb9c5536b31
xuansongqing <873357904@qq.com>
2019-12-31 10:18:38 +0800

    黑马头条-进度条

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,7 +1,11 @@
 // 处理器导航守卫
 import router from '../router'
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 router.beforeEach(function (to, from, next) {
+  progress.start() // 开启进度条
+  // 权限拦截 有token让进 无token不让进
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('user-token')
     if (token) {
@@ -12,4 +16,7 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+router.afterEach(() => {
+  progress.done()// 关闭进度条
 })
